7946d6d0623b68a52d8279b3541fba944e7ebdc8
yanhui2020 <wyd2014yan@163.com>
2020-04-13 12:17:58 +0800

    cron auto commit

--- single/json/posts.js
+++
@@ -1,6 +0,0 @@
-(function (v) {
-    v.prototype.$posts = [
-        { title: "Java8 Stream流式操作集合", overview: "集合分组，排序，过滤，去重，求和 ...", "link": "https://rwafvj2toy.feishu.cn/docs/doccnrPW4b9gNsHfFbtXramElFf" },
-        { title: "EasyPOI优雅的导入导出", overview: "利用easypoi快速完成日常的导入导出工作", "link": "https://rwafvj2toy.feishu.cn/docs/doccnJBZds1dzUAUEj7gC5DM4pf" },
-    ]
-})(Vue)
