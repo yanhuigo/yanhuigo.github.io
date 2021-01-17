7e48ccc426eb72f247101e604418de63863aba5b
hello <hello@gitee.com>
2021-01-17 12:15:51 +0800

    init

--- php/getRequest.php
+++
@@ -1,7 +0,0 @@
-<?
-echo($_POST['callbacks']);
-echo($_POST['total']);
-echo($_POST['paytime']);
-echo($_POST['order_sn']);
-echo($_POST['sign']);
-?>
--- push.bat
+++
@@ -1,3 +0,0 @@
-git add .
-git commit -m "script push"
-git push
