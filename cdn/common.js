39ca31befdfb53b7256aa165920df49469f91261
hello <hello@gitee.com>
2021-01-31 18:32:26 +0800

    cc

--- cdn/common.js
+++ cdn/common.js
@@ -1,7 +1,9 @@
-let common = (function () {
+let initAsyncFunc = async function (initCfg) {
 
+    let appCfg = {};
     const storage_login = "login-state";
     const storage_fileList = "fileList";
+    const configFilePath = "json/config.json";
     const config = {
         client_id: "f5250ed1c6f0a51423ca06aa4faf5c10d64ce8b411c425256d22fec16a531665",
         client_secret: "00a0f31357ce04fe3619eab7149d7c1b4daade23677a898b8f14bb647bc25fb3",
@@ -11,8 +13,6 @@ let common = (function () {
 
     if ("/login.html" === window.location.pathname) return {config};
 
-    axiosInit();
-
     let loginStorageData = localStorage.getItem(storage_login);
     if ("/login.html" !== window.location.pathname && !loginStorageData) {
         window.location.href = "/login.html";
@@ -20,17 +20,68 @@ let common = (function () {
     }
 
     let {access_token} = JSON.parse(loginStorageData);
-
     // 保存文件路径和sha的对应关系
     let fileTree = {};
-    let fileListStorageData = localStorage.getItem(storage_fileList);
-    if (fileListStorageData) {
-        fileTree = JSON.parse(fileListStorageData);
-    } else {
-        initFileTree().then(() => {
-            console.log("初始化文件列表完成...")
+
+    async function appInit() {
+
+        let baseLibs = ["/cdn/vue/vue.min.js", "/cdn/axios.min.js", "/cdn/bootstrap/jquery.slim.min.js"];
+        let asyncLibs = ["/cdn/bootstrap/bootstrap.min.js", "/cdn/bootstrap/bootstrap.min.css", "/cdn/font-awesome-4.7.0/css/font-awesome.min.css"];
+
+        if (initCfg.baseLibs) {
+            baseLibs = baseLibs.concat(initCfg.baseLibs);
+        }
+
+        if (initCfg.asyncLibs) {
+            asyncLibs = asyncLibs.concat(initCfg.asyncLibs);
+        }
+
+        await loadBaseLibs(baseLibs);
+        axiosInit();
+        for (let lib of asyncLibs) {
+            if (lib.indexOf(".css") !== -1) {
+                loadCss(lib);
+            } else {
+                loadLibs(lib);
+            }
+
+        }
+
+
+        return new Promise(async resolve => {
+            window.Vue && vueComponentBind();
+            await dataInit();
+            resolve();
         });
     }
+
+    async function loadBaseLibs(libs = []) {
+        for (let lib of libs) {
+            await loadBaseLib(lib);
+        }
+    }
+
+    async function loadBaseLib(lib) {
+        return new Promise((resolve) => {
+            loadLibs(lib, () => {
+                resolve();
+            });
+        });
+    }
+
+    async function dataInit() {
+        let fileListStorageData = localStorage.getItem(storage_fileList);
+        if (fileListStorageData) {
+            fileTree = JSON.parse(fileListStorageData);
+        } else {
+            await initFileTree();
+        }
+
+        // 初始化app配置
+        let appCfgData = await getContent(configFilePath);
+        appCfg = JSON.parse(appCfgData);
+    }
+
 
     function tip(msg, detail = "") {
         if (window.Notification) {
@@ -272,6 +323,129 @@ let common = (function () {
 
     }
 
+    function loadLibs(url, callback) {
+        let script = document.createElement('script');
+        let fn = callback || function () {
+        };
+
+        script.type = 'text/javascript';
+        if (script.readyState) {
+            script.onreadystatechange = function () {
+                if (script.readyState === 'loaded' || script.readyState === 'complete') {
+                    script.onreadystatechange = null;
+                    fn();
+                }
+            };
+        } else {
+            script.onload = function () {
+                fn();
+            };
+        }
+        script.src = url;
+        document.getElementsByTagName('head')[0].appendChild(script);
+    }
+
+    function loadCss(url, callback) {
+        let link = document.createElement('link');
+        let fn = callback || function () {
+        };
+
+        link.rel = 'stylesheet';
+        if (link.readyState) {
+            link.onreadystatechange = function () {
+                if (link.readyState === 'loaded' || link.readyState === 'complete') {
+                    link.onreadystatechange = null;
+                    fn();
+                }
+            };
+        } else {
+            link.onload = function () {
+                fn();
+            };
+        }
+        link.href = url;
+        document.getElementsByTagName('head')[0].appendChild(link);
+    }
+
+    function vueComponentBind() {
+
+        Vue.component('yanhui-header', {
+            data() {
+                return {
+                    links: appCfg.navLinks.concat([
+                        {
+                            title: "API操作", children: [
+                                {
+                                    title: "更新应用配置", call: () => {
+                                        if (confirm("确认清空应用配置并刷新页面？")) {
+                                            localStorage.removeItem(configFilePath);
+                                            window.location.reload();
+                                        }
+                                    }
+                                },
+                                {
+                                    title: "清空所有缓存", call: () => {
+                                        if (confirm("确认清空所有缓存并刷新页面？")) {
+                                            for (let key in localStorage) {
+                                                if (localStorage.hasOwnProperty(key) && key !== storage_login) {
+                                                    localStorage.removeItem(key);
+                                                }
+                                            }
+                                            window.location.reload();
+                                        }
+                                    }
+                                },
+                                {
+                                    title: "同步文件树", call: () => {
+                                        confirm("确认同步文件树？") && initFileTree().then(() => {
+                                            tip("同步文件树完成！");
+                                        });
+                                    }
+                                }
+                            ]
+                        },
+                    ])
+                }
+            },
+            methods: {},
+            mounted() {
+                console.log("yh header mounted");
+            },
+            props: ['title'],
+            template: `
+                <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                  <a class="navbar-brand" href="#">{{title}}</a>
+                  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                  </button>
+                  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                      
+                      <li class="nav-item" :class="link.children ? 'dropdown' : ''" v-for="(link,index) in links">
+                        <a v-if="!link.children" class="nav-link" :href="link.url">{{link.title}} <span class="sr-only">(current)</span></a>
+                        <template v-else>
+                            <a class="nav-link dropdown-toggle" href="#" :id="'navbarDropdown'+index" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                              {{link.title}}
+                            </a>
+                            <div v-if="link.children" class="dropdown-menu" :aria-labelledby="'navbarDropdown'+index">
+                                <template v-for="subLink in link.children">
+                                    <a v-if="subLink.call" class="dropdown-item" href="###" @click.prevent="subLink.call">{{subLink.title}}</a>
+                                    <a v-else class="dropdown-item" :href="subLink.url">{{subLink.title}}</a>
+                                </template>
+                            </div>
+                        </template>
+                        
+                      </li>
+                    </ul>
+                  </div>
+                </nav>
+            `
+        })
+
+    }
+
+    await appInit();
+
     return {
         config,
         fileTree,
@@ -280,7 +454,17 @@ let common = (function () {
         newFile,
         base64: Base64,
         tip,
-        initFileTree
+        initFileTree,
+        loadLibs
     }
 
-})();
+};
+
+
+window.initApp = async (initCfg = {}) => {
+
+    // 基础数据加载完成后再启动应用
+    window.common = await initAsyncFunc(initCfg);
+
+    console.log("init complete...");
+}
